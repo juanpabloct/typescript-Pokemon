f2fb4f5ed504b8aa8d9830a6b98a9d4f346dd20e
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-07-06 01:16:29 -0500

    se ha realizado la implementacion de nuevos reducers para filtros

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -1,2 +1,3 @@
 import pokemonsSlice from "../reducers/pokemon";
-export const { pokemonFilter, changeData } = pokemonsSlice.actions;
+export const { pokemonFilter, changeData, changeDataPagination, loginUser } =
+  pokemonsSlice.actions;
