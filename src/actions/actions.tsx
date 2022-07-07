b2ce6057951596fa2fbc203c6334a3bfb604f3c9
Juanpabloct <juanptorres2003@gmail.com>
2022-07-07 17:00:59 -0500

    se cambio el nombre a una accion de filtro

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -1,3 +1,2 @@
 import pokemonsSlice from "../reducers/pokemon";
-export const { pokemonFilter, changeData, changeDataPagination, loginUser } =
-  pokemonsSlice.actions;
+export const { pokemonFilter, changeData, changeDataPagination, loginUser } = pokemonsSlice.actions;
