f9608a9e99eee9fa6a49f55b054001e416dea835
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-07-07 22:48:37 -0500

    Se hizo un filtro

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,8 +1,12 @@
 import { Navbar } from "../components/Navbar";
 import { Filters } from "components/Filters";
 import { TargetData } from "components/ShowData/TargetData";
+import { useSelector } from "react-redux";
+import { ReduxState } from "types/store";
 
 function App() {
+  const filter = useSelector((state: ReduxState) => state.pokemon.filter);
+
   return (
     <div className="App">
       <Navbar />
